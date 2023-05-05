3c7950df93cb65fb112f7a7af572edcc4faabbab
Conner Bachmann <bac@christmann.info>
2023-05-05 13:23:06 +0200

    fix: merge conflicts

--- core/tables.go
+++ core/tables.go
@@ -2,6 +2,8 @@ package core
 
 import (
 	"strings"
+
+	"github.com/iskre/lib/helper"
 )
 
 type Table struct {
@@ -9,42 +11,43 @@ type Table struct {
 	Body    [][]string
 }
 
-func (i Isrke) Table(d *Table) string {
+func (i Iskre) Table(d *Table) string {
+	// TODO: add check for body length
 	// m = max length for full row
 	m := 0
 	b := strings.Builder{}
 
 	var headerLengths []int
 
-	b.WriteRune(TABLE_TOP_LEFT)
+	b.WriteRune(helper.TABLE_TOP_LEFT)
 
 	// write table header
 	for i, e := range d.Headers {
 		ml := len(e) + 8
 		m += ml
-		b.WriteString(strings.Repeat(string(TABLE_HORIZONTAL), ml))
+		b.WriteString(strings.Repeat(string(helper.TABLE_HORIZONTAL), ml))
 
 		headerLengths = append(headerLengths, ml)
 
 		if i != len(d.Headers)-1 {
-			b.WriteRune(TABLE_INTERSECTION_TOP)
+			b.WriteRune(helper.TABLE_INTERSECTION_TOP)
 		} else {
-			b.WriteRune(TABLE_TOP_RIGHT)
+			b.WriteRune(helper.TABLE_TOP_RIGHT)
 			b.WriteRune('\n')
 		}
 	}
 
 	for i, e := range d.Headers {
-		b.WriteRune(TABLE_VERTICAL)
+		b.WriteRune(helper.TABLE_VERTICAL)
 		b.WriteString("    " + e + "    ")
 		if i == len(d.Headers)-1 {
-			b.WriteRune(TABLE_VERTICAL)
+			b.WriteRune(helper.TABLE_VERTICAL)
 		}
 	}
 
 	b.WriteString("\n")
-	b.WriteRune(TABLE_INTERSECTION_LEFT)
-	b.WriteRune(TABLE_HORIZONTAL)
+	b.WriteRune(helper.TABLE_INTERSECTION_LEFT)
+	b.WriteRune(helper.TABLE_HORIZONTAL)
 
 	// write horizontal line to close table header
 	for i, e := range d.Headers {
@@ -54,18 +57,18 @@ func (i Isrke) Table(d *Table) string {
 			ml--
 		}
 
-		b.WriteString(strings.Repeat(string(TABLE_HORIZONTAL), ml))
+		b.WriteString(strings.Repeat(string(helper.TABLE_HORIZONTAL), ml))
 
 		if i == len(d.Headers)-1 {
-			b.WriteRune(TABLE_INTERSECTION_RIGHT)
+			b.WriteRune(helper.TABLE_INTERSECTION_RIGHT)
 		} else {
-			b.WriteRune(TABLE_INTERSECTION)
+			b.WriteRune(helper.TABLE_INTERSECTION)
 		}
 	}
 
 	for i, e := range d.Body {
 		b.WriteString("\n")
-		b.WriteRune(TABLE_VERTICAL)
+		b.WriteRune(helper.TABLE_VERTICAL)
 		for y, s := range e {
 			bodyString := " " + s + " "
 			b.WriteString(" " + s + " ")
@@ -73,9 +76,9 @@ func (i Isrke) Table(d *Table) string {
 			if len(bodyString) != headerLengths[y] {
 				b.WriteString(strings.Repeat(" ", headerLengths[y]-len(bodyString)))
 				if i == 0 {
-					b.WriteRune(TABLE_VERTICAL)
+					b.WriteRune(helper.TABLE_VERTICAL)
 				} else {
-					b.WriteRune(TABLE_VERTICAL)
+					b.WriteRune(helper.TABLE_VERTICAL)
 				}
 			}
 		}
@@ -83,15 +86,15 @@ func (i Isrke) Table(d *Table) string {
 		// write last line
 		if i == len(d.Body)-1 {
 			b.WriteString("\n")
-			b.WriteRune(TABLE_BOTTOM_LEFT)
+			b.WriteRune(helper.TABLE_BOTTOM_LEFT)
 
 			for y, e := range headerLengths {
-				b.WriteString(strings.Repeat(string(TABLE_HORIZONTAL), e))
+				b.WriteString(strings.Repeat(string(helper.TABLE_HORIZONTAL), e))
 
 				if y == len(headerLengths)-1 {
-					b.WriteRune(TABLE_BOTTOM_RIGHT)
+					b.WriteRune(helper.TABLE_BOTTOM_RIGHT)
 				} else {
-					b.WriteRune(TABLE_INTERSECTION_BOTTOM)
+					b.WriteRune(helper.TABLE_INTERSECTION_BOTTOM)
 				}
 			}
 
